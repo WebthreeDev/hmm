1b7ca9f17cad4ed7f4ca92605b8f16b882028323
Md. Aditya Amin <adittyaamin@gmail.com>
2021-12-18 01:04:56 +0600

    New Update

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -10,79 +10,54 @@ class Brand extends React.Component {
             speed: 1000,
             autoplay: true,
             arrows: false,
-            slidesToShow: 6,
+            slidesToShow: 2,
             slidesToScroll: 2,
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 5,
-                        slidesToScroll: 1,
-                        infinite: true,
-                    }
-                },
-                {
-                    breakpoint: 992,
-                    settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 767,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        arrows: false,
-                    }
-                },
-                {
-                    breakpoint: 575,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        arrows: false,
-                    }
-                },
-            ]
+            // responsive: [
+            //     {
+            //         breakpoint: 1200,
+            //         settings: {
+            //             slidesToShow: 5,
+            //             slidesToScroll: 1,
+            //             infinite: true,
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 992,
+            //         settings: {
+            //             slidesToShow: 4,
+            //             slidesToScroll: 1
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 767,
+            //         settings: {
+            //             slidesToShow: 3,
+            //             slidesToScroll: 1,
+            //             arrows: false,
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 575,
+            //         settings: {
+            //             slidesToShow: 2,
+            //             slidesToScroll: 1,
+            //             arrows: false,
+            //         }
+            //     },
+            // ]
         };
         return (
             <div className="brand-area brand-bg home-four-brand">
                 <div className="container">
-                    <Slider className="row brand-active" {...settings}>
+                    <Slider className="row brand-active align-items-center" {...settings}>
                         <div className="col-xl-2">
                             <div className="brand-item">
-                            <img src="assets/img/brand/brand_logo01.png" alt="BRAND" />
+                            <img src="assets/img/brand/certikwhite-1.png" alt="BRAND" />
                             </div>
                         </div>
                         <div className="col-xl-2">
                             <div className="brand-item">
-                            <img src="assets/img/brand/brand_logo02.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-2">
-                            <div className="brand-item">
-                            <img src="assets/img/brand/brand_logo03.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-2">
-                            <div className="brand-item">
-                            <img src="assets/img/brand/brand_logo04.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-2">
-                            <div className="brand-item">
-                            <img src="assets/img/brand/brand_logo05.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-2">
-                            <div className="brand-item">
-                            <img src="assets/img/brand/brand_logo06.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-2">
-                            <div className="brand-item">
-                            <img src="assets/img/brand/brand_logo03.png" alt="BRAND" />
+                            <img src="assets/img/brand/cmc.png" alt="BRAND" />
                             </div>
                         </div>
                     </Slider>
