9efcf0b3cfc271e9b0d718480d9f253252555e95
Md. Aditya Amin <adittyaamin@gmail.com>
2021-12-18 21:28:32 +0600

    Logos Updated

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -9,84 +9,75 @@ class Brands extends React.Component {
                     <div className="row brand-active align-items-center">
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand01.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand01.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand02.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand02.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand03.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand03.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand04.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand04.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand05.jpg" alt="BRAND" />
+                            <img src="assets/img/brand/brand05.jpg" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand06.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand06.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand07.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-3">
-                            <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand08.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-3">
-                            <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand09.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-3">
-                            <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand10.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        <div className="col-xl-3">
-                            <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand13.png" alt="BRAND" />
-                            </div>
-                        </div>
-                        
-                        <div className="col-xl-3">
-                            <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand14.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand07.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                       
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand16.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand10.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand17.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand13.png" alt="BRAND" className="img-fluid"/>
+                            </div>
+                        </div>
+                        
+                        <div className="col-xl-3">
+                            <div className="brand-item mt-4">
+                            <img src="assets/img/brand/brand14.png" alt="BRAND" className="img-fluid"/>
+                            </div>
+                        </div>
+                      
+                        <div className="col-xl-3">
+                            <div className="brand-item mt-4">
+                            <img src="assets/img/brand/brand16.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand18.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand17.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                         <div className="col-xl-3">
                             <div className="brand-item mt-4">
-                            <img src="assets/img/brand/brand19.png" alt="BRAND" />
+                            <img src="assets/img/brand/brand18.png" alt="BRAND" className="img-fluid"/>
+                            </div>
+                        </div>
+                        <div className="col-xl-3">
+                            <div className="brand-item mt-4">
+                            <img src="assets/img/brand/brand19.png" alt="BRAND" className="img-fluid"/>
                             </div>
                         </div>
                     </div>
